fc65d956c1b101dede51040ea9bfc3525f05db44
ryanleriche <leriche.ryan@gmail.com>
2022-07-06 12:06:13 -0700

    dividing pt analysis into individual scripts
    update from 07/06/22 pipeline dev meeting

--- working/RCS04_pain_metrics.m
+++ working/RCS04_pain_metrics.m
@@ -117,10 +117,11 @@ scbs_dir        = fullfile(patientroot_dir,...
 
 
 
+
 %% RCS04 plot daily metrics
 
 %{
-Specify, 'all-time' for a 5 nearest report sliding average of all daily
+Specify, 'AllTime' for a 5 nearest report sliding average of all daily
 reported pain metrics. Alternatively, to see the last n days of pain metrics
 specify 'PreviousDays' followed by n days (without the sliding average).
 %}
@@ -132,38 +133,17 @@ Add overlay of stim, days on patch, contacts, etc., as optional arguements
 % 07/02/22 RBL: consider adding 'cfg' input w/ stim specifications, and pt
 % ID as title
 
-% }
+%}
 
-
-plot_timeline(pt_pain.RCS04, 'all-time');
-
-plot_timeline(pt_pain.RCS04,'PreviousDays', 10);
-
+plot_timeline(pt_pain.RCS04, 'AllTime');
 
 % visually inspect to see distributions of the various pain metrics
 
-plot_hist(pt_pain.RCS04,'all-time');
+plot_hist(pt_pain.RCS04,'AllTime');
 
 
 % pain metric A "versus" pain metric B --> see how metrics visually covary
 
-plot_versus(pt_pain.RCS04, 'all-time');
-
-
-%% RCS05
-plot_timeline(pt_pain.RCS05, 'all-time');
-
-plot_hist(pt_pain.RCS05, 'all-time');
-
-
-
-
-
-%% RCS02
-plot_timeline(pt_pain.RCS02, 'all-time');
-
-plot_hist(pt_pain.RCS02, 'all-time');
-
-
+plot_versus(pt_pain.RCS04, 'AllTime');
 
 
